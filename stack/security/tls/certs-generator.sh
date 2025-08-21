93563e042a683c4c4987ebff26bf1cd3c49e6fca
Ubuntu <mozennou@wazuh-docker-swarm-cluster-master-VM-0.1y3znunt1hae3kz3uczq5n3aff.madx.internal.cloudapp.net>
2025-08-21 17:02:14 +0000

    Configure wazuh indexer in a single node

--- stack/security/tls/certs-generator.sh
+++ stack/security/tls/certs-generator.sh
@@ -6,3 +6,6 @@ docker run --rm \
   -v "$(pwd)/wazuh_ssl_certs/:/certificates/" \
   -v "$(pwd)/certs.yml:/config/certs.yml" \
   wazuh/wazuh-certs-generator:0.0.2
+
+chown -R 1000:1000 "$(pwd)/wazuh_ssl_certs/"
+chmod -R -600 "$(pwd)/wazuh_ssl_certs/"

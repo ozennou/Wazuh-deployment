9c96809edd2da931d7ee2922ce839adac55ce7d3
ozennou <ozennouamine02@gmail.com>
2025-08-27 10:28:03 +0000

    add nginx as reverse proxy

--- stack/security/tls/certs-generator.sh
+++ stack/security/tls/certs-generator.sh
@@ -7,5 +7,14 @@ docker run --rm \
   -v "$(pwd)/certs.yml:/config/certs.yml" \
   wazuh/wazuh-certs-generator:0.0.2
 
+echo "Generating nginx SSL certificates..."
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout $(pwd)/wazuh_ssl_certs/nginx.key \
+    -out $(pwd)/wazuh_ssl_certs/nginx.crt \
+    -subj "/CN=localhost"
+
+echo "Certificates generated"
+
 chown -R 1000:1000 "$(pwd)/wazuh_ssl_certs/"
 chmod -R -600 "$(pwd)/wazuh_ssl_certs/"
